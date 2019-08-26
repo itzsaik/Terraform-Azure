218d2fc00c3e73b9b30c37baff213745952d6516
Ubuntu <ladmin@terraform.4vstvfgcb4uerihms0oymwe22c.px.internal.cloudapp.net>
2019-08-26 01:55:54 +0000

    committing new changes

--- main.tf
+++ main.tf
@@ -28,6 +28,11 @@ provider "azurerm" {
   client_secret   = "${var.client_secret}"
 }
 
+locals {
+    web_server_name = "${var.environment == "production" ? "${var.web_server_name}-prd"  : "${var.web_server_name}-dev"}"
+}
+
+
 resource "azurerm_resource_group" "web_server_rg" {
   name     = "${var.web_server_rg}"
   location = "${var.web_server_location}"
@@ -48,30 +53,30 @@ resource "azurerm_subnet" "web_server_subnet" {
   network_security_group_id = "${azurerm_network_security_group.web_server_nsg.id}"
 }
 
-    resource "azurerm_network_interface" "web_server_nic" {
-      name                      = "${var.web_server_name}-${format("%02d",count.index)}-nic"
-      location                  = "${var.web_server_location}"
-      resource_group_name       = "${azurerm_resource_group.web_server_rg.name}"
-      count                     = "${var.web_server_count}"
+  // resource "azurerm_network_interface" "web_server_nic" {
+  //    name                      = "${var.web_server_name}-${format("%02d",count.index)}-nic"
+  //    location                  = "${var.web_server_location}"
+  //    resource_group_name       = "${azurerm_resource_group.web_server_rg.name}"
+  //    count                     = "${var.web_server_count}"
 
-      ip_configuration {
-        name                          = "${var.web_server_name}-${format("%02d",count.index)}-ip"
-        subnet_id                     = "${azurerm_subnet.web_server_subnet.id}"
-        private_ip_address_allocation = "dynamic"
-        public_ip_address_id          = "${azurerm_public_ip.web_server_public_ip.*.id[count.index]}"
-      }
-    }
+  //    ip_configuration {
+  //      name                          = "${var.web_server_name}-${format("%02d",count.index)}-ip"
+  //      subnet_id                     = "${azurerm_subnet.web_server_subnet.id}"
+  //      private_ip_address_allocation = "dynamic"
+  //      public_ip_address_id          = "${azurerm_public_ip.web_server_public_ip.*.id[count.index]}"
+  //    }
+   // }
 
     resource "azurerm_public_ip" "web_server_public_ip" {
-      name                         = "${var.web_server_name}-${format("%02d",count.index)}-public-ip"
+      name                         = "${var.resource_prefix}-public-ip"
       location                     = "${var.web_server_location}"
       resource_group_name          = "${azurerm_resource_group.web_server_rg.name}"
       allocation_method            = "Static"
-      count                        = "${var.web_server_count}"
+      
     }
 
     resource "azurerm_network_security_group" "web_server_nsg" {
-      name                = "${var.web_server_name}-nsg"
+      name                = "${var.resource_prefix}-nsg"
       location            = "${var.web_server_location}"
       resource_group_name = "${azurerm_resource_group.web_server_rg.name}" 
     }
@@ -90,41 +95,44 @@ resource "azurerm_subnet" "web_server_subnet" {
       network_security_group_name = "${azurerm_network_security_group.web_server_nsg.name}" 
     }
 
-  resource "azurerm_availability_set" "webserver_availability_set" {
-    name                         = "${var.resource_prefix}-availability-set"
-    location                     = "${var.web_server_location}"
-    resource_group_name          = "${azurerm_resource_group.web_server_rg.name}"
-    managed                      = true
-    platform_fault_domain_count  = 2
+//  resource "azurerm_availability_set" "webserver_availability_set" {
+//    name                         = "${var.resource_prefix}-availability-set"
+//    location                     = "${var.web_server_location}"
+//    resource_group_name          = "${azurerm_resource_group.web_server_rg.name}"
+//    managed                      = true
+//    platform_fault_domain_count  = 2
 
-}
+//}
 
-resource "azurerm_virtual_machine" "web_server" {
-    name                         = "${var.web_server_name}-${format("%02d",count.index)}"
+resource "azurerm_virtual_machine_scale_set" "web_server" {
+    name                         = "${local.web_server_name}-scale-set"
     location                     = "${var.web_server_location}"
     resource_group_name          = "${azurerm_resource_group.web_server_rg.name}"  
-    network_interface_ids        = ["${azurerm_network_interface.web_server_nic.*.id[count.index]}"]
-    vm_size                      = "Standard_F2s_v2"
-    availability_set_id          = "${azurerm_availability_set.webserver_availability_set.id}"
-    count                        = "${var.web_server_count}"
+    upgrade_policy_mode          = "manual"
+
+    sku {
+     name                        = "Standard_F2s_v2"
+     tier                        = "Standard"
+     capacity                    = "${var.web_server_count}" 
+    }
 
 
-    storage_image_reference {
+    storage_profile_image_reference {
       publisher = "MicrosoftWindowsServer"
       offer     = "WindowsServer"
       sku       = "2016-Datacenter-Server-Core-smalldisk"
       version   = "latest"
     }
 
-    storage_os_disk {
-      name              = "${var.web_server_name}-${format("%02d",count.index)}-os"    
+    storage_profile_os_disk {
+      name              = ""
       caching           = "ReadWrite"
       create_option     = "FromImage"
       managed_disk_type = "Standard_LRS"
     }
     
     os_profile {
-      computer_name      = "${var.web_server_name}-${format("%02d",count.index)}" 
+      computer_name_prefix      = "${local.web_server_name}" 
       admin_username     = "webserver"
       admin_password     = "Passw0rd1234"
     }
@@ -132,4 +140,15 @@ resource "azurerm_virtual_machine" "web_server" {
     os_profile_windows_config {
     }
 
+    network_profile{
+      name  ="web_server_network_profile"
+      primary = true
+
+    ip_configuration{
+      name =  "${local.web_server_name}"
+      primary = true
+      subnet_id = "${azurerm_subnet.web_server_subnet.*.id[0]}"
+    }
+    }
 }
+
